7487c7964d094f6c00c89ff8c493401eea2787b8
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-04-27 21:49:24 -0600

    Fixed page movement with circle size changes and places

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -329,3 +329,40 @@ function minusCalDay() {
 }
 
 setDates();
+
+function cicleLocations() {
+  let xRandNum = Math.floor(Math.random() * 300) + 100;
+  let yRandNum = Math.floor(Math.random() * 50) + 20;
+  let xRandNum2 = Math.floor(Math.random() * 300) + 100;
+  let yRandNum2 = Math.floor(Math.random() * 50) + 20;
+
+  let circle1 = document.querySelector(".circle1");
+  let circle2 = document.querySelector(".circle2");
+
+  circle1.setAttribute(
+    "style",
+    "margin-top: " +
+      xRandNum +
+      "px; margin-left: " +
+      yRandNum +
+      "%; overflow: hidden; height: " +
+      xRandNum +
+      "px; width: " +
+      xRandNum +
+      "px;"
+  );
+  circle2.setAttribute(
+    "style",
+    "margin-top: " +
+      xRandNum2 +
+      "px; margin-left: " +
+      yRandNum2 +
+      "%; overflow: hidden; height: " +
+      xRandNum2 +
+      "px; width: " +
+      xRandNum2 +
+      "px;"
+  );
+}
+
+cicleLocations();

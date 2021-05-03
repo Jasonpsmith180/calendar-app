22430c84ac666f60560de9c7825d3acad20e21b1
Jason <jasonpsmith180@gmail.com>
2021-05-03 17:22:39 -0600

    add more npm packages

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,16 @@
-hello world
+// import sequelize constructor
+const Sequelize = require('sequelize');
+
+// dotenv dependency for env variables
+require('dotenv').config();
+
+// create connection to db
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+module.exports = sequelize;

557987d0624ea263141bce8d94cb9333a956d060
Jason <jasonpsmith180@gmail.com>
2021-05-03 18:10:48 -0600

    fixing merge conflicts

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+// dependencies
 const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const session = require("express-session");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const exphbs = require("express-handlebars");
 
-const session = require("express-session");
+const routes = require("./controllers");
+const moment = require("moment");
+const Year = require("./assets/js/year.json");
 
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
@@ -25,49 +28,42 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-
-const moment = require("moment");
-const express = require("express");
-const path = require("path");
-const app = express();
-const Year = require("./assets/js/year.json");
-const exphbs = require('express-handlebars');
-const port = process.env.PORT || 3000;
-
 app.use(express.static("assets"));
 app.use(express.static(path.join(__dirname, "assets")));
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.use(routes);
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
+  });
+  
+  app.post("/calendar", function (req, res) {
+    res.sendFile(path.join(__dirname, "/assets/pages/calendar.html"));
+  });
+  
+  app.get("/appointments", function (req, res) {
+    res.sendFile(path.join(__dirname, "/assets/pages/appointments.html"));
+  });
+  
+  app.post("/credentials", function (req, res) {
+    res.sendFile(path.join(__dirname, "/assets/pages/creds.html"));
+  });
+  
+  app.get("/list", function (req, res) {
+    res.sendFile(path.join(__dirname, "/assets/pages/list.html"));
+  });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
 
-app.post("/calendar", function (req, res) {
-  res.sendFile(path.join(__dirname, "/assets/pages/calendar.html"));
-});
 
-app.get("/appointments", function (req, res) {
-  res.sendFile(path.join(__dirname, "/assets/pages/appointments.html"));
-});
 
-app.post("/credentials", function (req, res) {
-  res.sendFile(path.join(__dirname, "/assets/pages/creds.html"));
-});
 
-app.get("/list", function (req, res) {
-  res.sendFile(path.join(__dirname, "/assets/pages/list.html"));
-});
-////////////////////////////////
-app.listen(port, function () {
-  console.log("Listening");
-});
